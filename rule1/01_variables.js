d8283b474ad047ca53788e4b734cbb57a709d7d0
avanish7390 <singhavanish7700@gmail.com>
2024-03-19 00:12:07 +0530

    learn variables

---
+++ rule1/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144552
+let accountEmail = "avansih@gmail.com"  // it is solve the scope problem
+var accountPassword = "avanish55" 
+accountCity = "lucknow"
+let accountState // only declare variables not assigned any value then give the output undefined
+// it is undefined 
+
+// accountId = 22 //const not allowed to change account
+
+accountEmail = "asdf@gmail.com"
+accountPassword = "55255226"
+accountCity = "mirzapur"
+
+
+/* commant */
+// prefer not to use var //
+//because OF issue in block scope ans functional scope
+
+
+// console.log(accountId, accountEmail, accountPassword, accountCity); //it give  the output in row formate
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);  // it give the output in table formate
