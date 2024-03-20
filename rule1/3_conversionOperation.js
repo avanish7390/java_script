cd5cb1cf10d42e12b0655943cc61c9376e68f22a
avanish7390 <singhavanish7700@gmail.com>
2024-03-21 01:44:43 +0530

    conversion

---
+++ rule1/3_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = 388
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+ // type of number
+
+
+ let score = null // output 0
+
+ console.log(typeof score); // string
+
+
+
+ let valueInNumber = Number(score)// number
+ console.log(typeof valueInNumber); // number
+
+ console.log(valueInNumber);
+
+
+//  let score = "33ssjd"
+//  console.log(valueInNumber);  // NaN
