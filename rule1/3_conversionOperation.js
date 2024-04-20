10bd45c97c9b44bf37c8aea59d3c243b794db478
avanish7390 <singhavanish7700@gmail.com>
2024-04-20 22:27:52 +0530

    conplete

--- rule1/3_conversionOperation.js
+++ rule1/3_conversionOperation.js
@@ -1,12 +1,14 @@
 // let score = 388
 
 
+
 // console.log(typeof score);
 // console.log(typeof(score));
  // type of number
 
 
- let score = null // output 0
+ let score = null // output    0
+//  let score = undefined // output  undefined
 
  console.log(typeof score); // string
 
@@ -18,5 +20,44 @@
  console.log(valueInNumber);
 
 
+
 //  let score = "33ssjd"
 //  console.log(valueInNumber);  // NaN
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+
+
+// let isLoggedIn = 1 //true
+//let isLoggedIn ="" // false
+
+let isLoggedIn = "avanish"  //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
+
+//note 
+// 1 => true
+// 0 => false
+// "" => false
+// "avanish" => true
+
+let someNumber = 58265
+let stringNumber = String(someNumber)
+//console.log(stringNumber); //58265
+// console.log(typeof (stringNumber)); // string
+
+let someName = "avanish"
+let booleanName = Boolean(someName)
+// console.log(booleanName);// true
+// console.log(typeof booleanName); // boolean
+
+
+
+
